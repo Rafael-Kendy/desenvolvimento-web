465b8311b3f45500ece69c0dd12dc19dcff0dddf
Kendy93 <rafaelknlopes65@gmail.com>
2025-10-11 16:20:35 -0300

    Testando a fastAPI

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,22 +1,43 @@
+import uvicorn #pra rodar
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware #pra ligar o front com o back
+from fastapi.middleware.cors import CORSMiddleware #liga front e back
+from pydantic import BaseModel #pros tipos das coisas
+from typing import List
+
+#Kendy: algumas coisas enquanto to aprendendo, vou deixar q talvez seja util dps
+#   retirado de https://www.youtube.com/watch?v=aSdVU9-SxH4
+class Fruit(BaseModel):
+    name: str
+class Fruits(BaseModel):
+    frutas: List[Fruit]
 
 app = FastAPI()
 
-#faz a ligação com o front
+origins = [
+    "http://localhost:5173/",
+    "http://localhost:8000/"
+]
+
+#vai permitir qlqr URL vindo da origem
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], #servidor do react
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-#endpoints
-@app.get("/")
-def root(): #root só pra teste
-    return {"message": "Bem vindo ao site"}
+#db em memoria, so pra testar
+memory_db={"frutas":[]}
 
-@app.get("/api/teste")
-def get_data():
-    return {"name": "FastAPI", "language": "Python", "status": "Rodando"}
+@app.get("/fruits", response_model=Fruits) #response_model é o tipo q vai retornar
+def getFruits():
+    return Fruits(frutas=memory_db["frutas"])
+
+@app.post("/fruits", response_model=Fruit)
+def addFruit(fruta: Fruit):
+    memory_db["frutas"].append(fruta)
+    return fruta
+
+if __name__=="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
