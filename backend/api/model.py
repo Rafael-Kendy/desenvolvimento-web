30f24e11cff774ee6d8d8273c9704d3d84bd1be0
HAMassaro <116125928+HAMassaro@users.noreply.github.com>
2025-11-20 14:48:24 -0300

    criando tabela de usuarios

--- backend/api/model.py
+++ backend/api/model.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from pydantic import EmailStr
 
 class Question(SQLModel, table=True):  #table=true pra falar que e uma tabela db real
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -8,3 +9,12 @@ class Question(SQLModel, table=True):  #table=true pra falar que e uma tabela db
     title: str
     question: str
     image_url: Optional[str] = None
+
+#tabela de users
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    email: EmailStr = Field(index=True, unique=True)#index=True acelera a busca, unique=True impede emails repetidos
+    hashed_password: str
+    description: Optional[str] = "Salve, novo usuario criado"
+    is_premium: bool = Field(default=False)
