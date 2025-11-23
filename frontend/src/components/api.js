b15761cde1baec3b1a8b216c42fa498a83c17177
HAMassaro <116125928+HAMassaro@users.noreply.github.com>
2025-11-23 13:32:59 -0300

    url com o link gerado pelo render

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api=axios.create({
-    baseURL: "http://localhost:8000" //url do backend
+   //baseURL: "http://localhost:8000" //url do backend
+     baseURL: "https://chave-digital.onrender.com" //link da hospedagem feita com o render
 });
 
 export default api;
