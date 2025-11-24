35c7eb9d6d9b102cdfe0a83e0ae8152e1564b144
Rafael-Kendy <116125745+Rafael-Kendy@users.noreply.github.com>
2025-11-24 14:19:44 -0300

    Revertendo b15761cde1baec3b1a8b216c42fa498a83c17177

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-   //baseURL: "http://localhost:8000" //url do backend
-     baseURL: "https://chave-digital.onrender.com" //link da hospedagem feita com o render
+     baseURL: "http://localhost:8000" //url do backend
 });
 
 export default api;
