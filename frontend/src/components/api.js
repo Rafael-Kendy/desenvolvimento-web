f9351668a523f1aa5723dcb1976ee7d6de504eb2
Rafael-Kendy <116125745+Rafael-Kendy@users.noreply.github.com>
2025-11-25 20:53:35 -0300

    Mudado link do backend

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-     baseURL: "http://localhost:8000" //url do backend
+     baseURL: "https://desenvolvimento-web-ibke.onrender.com" //url do backend
 });
 
 export default api;
