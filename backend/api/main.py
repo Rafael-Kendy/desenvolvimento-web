d5d767a2d9f5e65cf8e837d88b0c0e2eda8b27d0
Rafael-Kendy <116125745+Rafael-Kendy@users.noreply.github.com>
2025-11-25 20:30:43 -0300

    Mudado link do backend

--- backend/api/main.py
+++ backend/api/main.py
@@ -32,7 +32,7 @@ print("\nDebug: Server iniciou\n")
 origins = [
     "http://localhost:5173", #front
     "http://localhost:8000", #back
-    "https://chave-digital.onrender.com", #back hosteado no render
+    "https://desenvolvimento-web-ibke.onrender.com", #back hosteado no render
     "https://desenvolvimento-web-frontend.onrender.com" #front do render
 ]
 
