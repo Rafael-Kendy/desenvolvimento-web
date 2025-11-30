88e85729465a26a482c9685741e650097b241dff
HAMassaro <116125928+HAMassaro@users.noreply.github.com>
2025-11-30 20:59:31 -0300

    url atualizada

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-     baseURL: "https://desenvolvimento-web-ibke.onrender.com" //url do backend
+     baseURL: "https://chave-digital.onrender.com" //url do backend
 });
 
 export default api;
