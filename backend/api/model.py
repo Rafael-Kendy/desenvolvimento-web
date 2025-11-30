39e3fce0743ea230d4a41a63f9df8c0274c13f03
HAMassaro <116125928+HAMassaro@users.noreply.github.com>
2025-11-30 20:54:09 -0300

    com comentarios atualizados

--- backend/api/model.py
+++ backend/api/model.py
@@ -10,9 +10,10 @@ class Question(SQLModel, table=True):  #table=true pra falar que e uma tabela db
     question: str
     image_url: Optional[str] = None
 
-#tabela de users
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+#tabela de users, modelagem do bd
+#ORM é o sqlmodel, permite usar objetos de python normal e dps traduz pro sql, uma busca de usuario se traduziria em select
+class User(SQLModel, table=True):#true diz q eh uma tabela real
+    id: Optional[int] = Field(default=None, primary_key=True)#primary_key=true pra dizer que o bd eh o responsavel por gerar o id
     name: str
     email: EmailStr = Field(index=True, unique=True)#index=True acelera a busca, unique=True impede emails repetidos
     hashed_password: str
