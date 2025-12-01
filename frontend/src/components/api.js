f9848cad365dc18521ab773279362e0dce2f349d
Rafael Watanabe <rafael.zaupanabe@gmail.com>
2025-12-01 04:32:43 -0300

    Revertendo mudança ruim
    Mudei de volta pra teste local. Agora deve dar.

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api=axios.create({
-     baseURL: "http://localhost:8000", //para testes locais
-     //baseURL: "https://chave-digital.onrender.com" //url do backend
+     //baseURL: "http://localhost:8000", //para testes locais
+     baseURL: "https://chave-digital.onrender.com" //url do backend
 });
 
 export default api;
